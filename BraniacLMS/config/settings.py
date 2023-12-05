b306a8fb7e9f61e205497f7326e59ae48282ba85
nomad <Pardus92@yandex.ru>
2023-12-05 22:31:52 +0300

    создание записи логов

--- BraniacLMS/config/settings.py
+++ BraniacLMS/config/settings.py
@@ -162,3 +162,30 @@ SOCIAL_AUTH_GITHUB_SECRET = "e85d674e5628f03d3b58e6d18ed0b9d4bd14b184"
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 
+LOG_FILE = BASE_DIR / "var" / "log" / "main_log.log"
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "console": {
+            "format": "[%(asctime)s] %(levelname)s %(name)s (%(lineno)d) %(message)s"
+        },
+    },
+    "handlers": {
+        "file": {
+            "level": "DEBUG",
+            "class": "logging.FileHandler",
+            "filename": LOG_FILE,
+            "formatter": "console",
+        },
+        "console": {"class": "logging.StreamHandler", "formatter": "console"},
+    },
+    "loggers": {
+        "django": {"level": "INFO", "handlers": ["console"]},
+        "mainapp": {
+            "level": "DEBUG",
+            "handlers": ["file"],
+        },
+    },
+}
